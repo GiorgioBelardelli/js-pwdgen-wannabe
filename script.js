1b7d236110ad4cf292d8f3034fcebd5dd94c4044
GiorgioBelardelli <asterapocalypse@gmail.com>
2023-11-16 15:23:51 +0100

    Es. finito

--- script.js
+++ script.js
@@ -1,6 +1,10 @@
-const userAge = parsInt(prompt("Quanti anni hai?"));
-const userColor = ;
-const userName = ;
-const userSurname = ;
+const userName = (prompt("Inserisci il nome"));
+console.log(userName);
 
-console.log(userEta);
+const userSurname = (prompt("Inserisci il cognome"));
+console.log(userSurname);
+
+const userColor = (prompt("Scrivi il tuo colore preferito"));
+console.log(userColor);
+
+document.getElementById("psw-sicura").innerHTML = "Ecco la tua password sicurissima: " + userName + userSurname + userColor + '23';
